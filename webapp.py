9d6bb11d76e3cfff6658cced75b81f58108b66e6
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-04-09 09:42:03 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,1 +1,12 @@
+from flask import Flask, request, Markup, render_template
+import os
+import json
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':    
+    app.run(debug=False)
+        

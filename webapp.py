4357267bb855f3361b1b6bc59bc5d522252acfbc
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-04-09 10:00:04 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/p1")
+def render_page1():
+    return render_template('page1.html')
+
+@app.route("/p2")
+def render_page2():
+    return render_template('page2.html')
+
 if __name__ == '__main__':    
     app.run(debug=False)
         

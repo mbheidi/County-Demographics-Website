5e1934787be9318323df31eca20f892c4d8740c9
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-04-13 09:25:01 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,15 +5,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('author.html')
 
 @app.route("/p1")
 def render_page1():
-    return render_template('page1.html')
+    return render_template('index.html')
 
 @app.route("/p2")
 def render_page2():
-    return render_template('page2.html')
+    return render_template('subject.html')
+
+@app.route("/p3")
+def render_page2():
+    return render_template('title.html')
+
+@app.route("/p4")
+def render_page2():
+    return render_template('year.html')
 
 if __name__ == '__main__':    
     app.run(debug=False)
